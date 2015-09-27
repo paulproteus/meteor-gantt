5affb3986261f0c8d003dc41da782d1167f296e5
Maik Mettenheimer <mettenheimer@pausanio.de>
2015-09-27 13:53:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Meteor Gantt
+===
+
+Simple interactive Gantt Chart for Project Planning.
+
+![Demo Screenshot](/docs/screenshot.png)
+
+Libraries and Docs
+---
+- http://docs.dhtmlx.com  
+- http://meteortips.com  
+- https://atmospherejs.com/stubborncoder/bootstrap-material-datepicker  
+- https://fezvrasta.github.io/bootstrap-material-design  
+
+Todo
+---
+- User Management (only one user is created on first start up)  
+- Gantt UI with material designed elements  
---
+++ code/app.js
@@ -0,0 +1,143 @@
+
+TasksCollection = new Mongo.Collection("tasks");
+LinksCollection = new Mongo.Collection("links");
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/login', {
+  name: 'login',
+  template: 'login'
+});
+
+Router.route('/about', {
+  name: 'about',
+  template: 'about'
+});
+
+Router.route('/', {
+    name: 'home',
+    template: 'home',
+    data: function() {},
+    onBeforeAction: function(){
+        var currentUser = Meteor.userId();
+        if(currentUser){
+            this.next();
+        } else {
+            this.redirect('/login');
+        }
+    },
+    onAfterAction: function(){
+      var currentUser = Meteor.userId();
+      if(currentUser){
+        setTimeout(function()
+        {
+          var daysStyle = function(date){
+              var dateToStr = gantt.date.date_to_str("%D");
+              if (dateToStr(date) == "Sun" || dateToStr(date) == "Sat") {
+                return "weekend";
+              }
+              return "";
+          };
+          gantt.config.start_date = new Date(2015,7,1);
+          gantt.config.end_date = new Date(2016,9,1);
+          gantt.config.scale_height = 84;
+          gantt.config.scale_unit = "month";
+          gantt.config.date_scale = "%F, %Y";
+          gantt.config.grid_width = 300;
+          gantt.config.min_column_width = 20;
+          gantt.config.subscales = [
+              {unit:"week", step:1, date:"Week %W"},
+              {unit:"day", step:1, date:"%d", css:daysStyle}
+          ];
+          gantt.config.columns =  [
+              {name: "text", label: "Tasks",  width: '*', tree: true },
+              {name: "add", label:"", width: 44 }
+          ];
+          gantt.init("gantt_here");
+          // Init dhtmlxGantt data adapter.
+          gantt.meteor({tasks: TasksCollection, links: LinksCollection});
+        }, 0);
+      }
+    },
+});
+
+if (Meteor.isServer) {
+
+  Meteor.startup(function() {
+    // create default user
+    if (Meteor.users.find().count() == 0) {
+      Accounts.createUser({
+          email: 'admin@mail.com',
+          password: 'admin'
+      });
+    }
+  });
+
+  Meteor.publish('tasks', function(){
+      return TasksCollection.find();
+  });
+  Meteor.publish('links', function(){
+      return LinksCollection.find();
+  });
+
+}
+
+if (Meteor.isClient) {
+
+    Meteor.subscribe('tasks');
+    Meteor.subscribe('links');
+
+    Template.navigation.events({
+        'click .logout': function(event){
+            event.preventDefault();
+            Meteor.logout();
+        }
+    });
+
+    Template.login.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(email, password, function(error){
+                if(error){
+                    console.log(error.reason);
+                } else {
+                    Router.go("/");
+                }
+            });
+        }
+    });
+
+    Template.navigation.helpers({
+      isActiveRoute:function(page){
+        if (page == Router.current().route.getName()) {
+          return 'active';
+        }
+      }
+    });
+
+    Template.home.events({
+      'focus #date_start': function(e, template){
+          var f = Template.instance().$('#date_start');
+          f.bootstrapMaterialDatePicker({format:'YYYY-MM-DD', time: false});
+      },
+      'focus #date_end': function(e, template){
+          var f = Template.instance().$('#date_end');
+          f.bootstrapMaterialDatePicker({format:'YYYY-MM-DD', time: false});
+      },
+      'change #date_start': function(e, template){
+          var date_start = $('[name="date_start"]').val().split("-");
+          gantt.config.start_date = new Date(date_start[0], date_start[1], date_start[2]);
+          gantt.render();
+      },
+      'change #date_end': function(e, template){
+          var date_end = $('[name="date_end"]').val().split("-");
+          gantt.config.end_date = new Date(date_end[0], date_end[1], date_end[2]);
+          gantt.render();
+      }
+    });
+
+}
